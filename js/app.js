d3e11af3f96cdde59dbb3092119affc5c3d9d39b
PaolaFU <paola_sfu@hotmail.com>
2017-11-18 19:59:36 -0500

    Modificando estructura html y agregando version 0.0.2

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@
 window.addEventListener('load', function() {
   var txtTwittear = document.getElementById('msj-twittear');
   var buttonTwitter = document.getElementById('btn-twittear');
-  var letters = document.getElementById('cont-letters');
+  var letters = document.getElementById('count-letters');
   var containerTwitter = document.getElementById('section-container');
+
+  var counter = 0;
   // Evento click para el boton Twittear
   buttonTwitter.addEventListener('click', addTwitter);
-    
+
+  txtTwittear.addEventListener('keyup', deshabilityBtn);
+  // txtTwittear.addEventListener('keydown', contLetters);
+
   // Funcion para agregar un nuevo tweet
   function addTwitter(event) {
     event.preventDefault();
@@ -16,6 +21,22 @@ window.addEventListener('load', function() {
     tweetbox.appendChild(tweet);
     containerTwitter.insertBefore(tweetbox, containerTwitter.firstElementChild);
     tweetbox.classList.add('tweets');
+    // Para volver  al inicio, sin ningun mensaje escrito
+    txtTwittear.value = '';
+    buttonTwitter.className = 'hidden';
+    letters.textContent = 140;
+    counter = 0;
+    deshabilityBtn(event);
+  }
+  // funcion para deshabilitar  boton, sino se escribe nada
+  function deshabilityBtn(event) {
+    event.preventDefault();
+    counter = txtTwittear.value.length;
+    letters.textContent = 140 - counter;
+
+    if (counter > 0 && counter <= 140) {
+      buttonTwitter.disabled = false;
+    } else buttonTwitter.disabled = true;
   }
 });
   
